312555a6aeea765450d35f21c10f28324e487296
daboggg <daboggg@ya.ru>
2020-02-20 09:47:05 +0300

    init

---
+++ src/main/java/ru/zinin/myhelper/MyhelperApplication.java
@@ -0,0 +1,13 @@
+package ru.zinin.myhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zinin/myhelper/MyhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zinin.myhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
